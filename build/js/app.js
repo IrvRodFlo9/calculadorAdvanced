900f15f4069f2b2ac0a1883603cf4196378feb2f
IrvRodFlo9JU <irvrodflo9@gmail.com>
2023-10-15 18:49:02 -0600

    created buttons

--- build/js/app.js
+++ build/js/app.js
@@ -1,2 +1,2 @@
-console.log("hello word");
+import{buttons}from"./data.js";const btnContainer=document.querySelector(".buttons-container");buttons.forEach(t=>{const n=document.createElement("button");n.innerHTML=t,btnContainer.appendChild(n)});
 //# sourceMappingURL=app.js.map
---
+++ build/js/data.js
@@ -0,0 +1,2 @@
+export const buttons=["sin","cos","tan","ln","log","x<sup>2</sup>","x<sup>-1</sup>","Ans","(",")","Del","Clear","7","8","9","+","4","5","6","x","1","2","3","/","0",".","=","-"];
+//# sourceMappingURL=data.js.map
